7a4ba08286c0c0d431c85cb6b800a9071e071ad3
empeje <11813607+empeje@users.noreply.github.com>
2019-09-02 20:00:44 +0700

    Exercise implementing stack

---
+++ src/dataStructure.js
@@ -0,0 +1,57 @@
+/** Class representing a Stack. */
+class Stack {
+
+  constructor() {
+    this._storage = {};
+    this._size = 0;
+  }
+  /*
+  * Adds a new value at the end of the stack
+  * @param {*} value the value to push
+  */
+  push(value) {
+    this._size ++;
+    this._storage[this._size] = value;
+  }
+
+  /*
+  * Removes the value at the end of the stack and returns it
+  * @return {*} the last and newest value in the stack
+  */
+  pop() {
+    if(this._size === 0) return null;
+
+    const results = this._storage[this._size];
+
+    delete this._storage[this._size];
+    this._size --;
+    
+    return results;
+  }
+  /*
+  * Returns the value at the end of the stack without removing it
+  * @return {*} the last and newest value in the stack
+  */
+  peek() {
+    return this._storage[this._size];
+  }
+
+  size() {
+    return this._size;
+  }
+
+  empty() {
+    this._storage = {};
+    this._size = 0;
+  }
+
+  swap() {
+    const temp = this._storage[this._size];
+    this._storage[this._size] = this._storage[this._size - 1];
+    this._storage[this._size - 1] = temp;
+  }
+}
+
+export {
+  Stack
+}
---
+++ test/dataStructure.test.js
@@ -0,0 +1,73 @@
+import { expect } from 'chai';
+import { describe, beforeEach } from 'mocha'
+import {
+  Stack
+} from "../src/dataStructure";
+
+describe('data structure implementation', () => {
+  describe('stack', () => {
+    let newStack;
+
+    beforeEach(() => {
+      newStack = new Stack();
+    });
+
+    it('should be able to push value', () => {
+      newStack.push('test value');
+      expect(newStack._storage[newStack._size]).to.equal('test value');
+    });
+
+    it('should be able to push more than one value', () => {
+      newStack.push('test value 1');
+      expect(newStack._storage[newStack._size]).to.equal('test value 1');
+      newStack.push('test value 2');
+      expect(newStack._storage[newStack._size]).to.equal('test value 2');
+    });
+
+    it('should be able to pop value', () => {
+      newStack.push('test value 1');
+      expect(newStack._storage[newStack._size]).to.equal('test value 1');
+      newStack.push('test value 2');
+      expect(newStack._storage[newStack._size]).to.equal('test value 2');
+
+      expect(newStack.pop()).to.equal('test value 2');
+      expect(newStack._storage[newStack._size]).to.equal('test value 1');
+    });
+
+    it('should be able to peek value', () => {
+      newStack.push('test value 1');
+      expect(newStack.peek()).to.equal('test value 1');
+      newStack.push('test value 2');
+      expect(newStack.peek()).to.equal('test value 2');
+    });
+
+    it('should be able to see stack size', () => {
+      newStack.push('test value 1');
+      newStack.push('test value 2');
+      expect(newStack.size()).to.equal(2);
+    });
+
+    it('should be able to empty stack', () => {
+      newStack.push('test value 1');
+      newStack.push('test value 2');
+      expect(newStack.size()).to.equal(2);
+      newStack.empty();
+      expect(newStack.size()).to.equal(0);
+    });
+
+    it('should not be able to pop empty stack', () => {
+      expect(newStack.pop()).to.be.null;
+    });
+
+    it('should be able to swap two top most elements', () => {
+      newStack.push('test value 1');
+      newStack.push('test value 2');
+      expect(newStack.peek()).to.equal('test value 2');
+
+      newStack.swap();
+      expect(newStack.peek()).to.equal('test value 1');
+      expect(newStack._storage[newStack.size()]).to.equal('test value 1');
+      expect(newStack._storage[newStack.size() - 1]).to.equal('test value 2');
+    });
+  });
+});

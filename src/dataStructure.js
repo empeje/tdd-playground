fce59c48d0b31c1e3c54a33f41f23e5ec6ee4a3b
empeje <11813607+empeje@users.noreply.github.com>
2019-09-04 13:07:52 +0700

    Refactor remove tail linked list

--- src/dataStructure.js
+++ src/dataStructure.js
@@ -144,15 +144,12 @@ class LinkedList {
   * @return {*} - the removed value
   */
   removeTail() {
-    let beforeTail;
-
-    let node = this.head;
-    while(!beforeTail) {
-      if(node.next === this.tail) beforeTail = node;
-      node = node.next;
+    let currentNode = this.head;
+    while(currentNode.next !== this.tail) {
+      currentNode = currentNode.next;
     }
 
-    this.tail = beforeTail;
+    this.tail = currentNode;
     this.tail.next = null;
   }
 

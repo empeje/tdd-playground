dced47bdeaf9c1f949a50863def3260a635d250d
mpj <11813607+empeje@users.noreply.github.com>
2021-07-26 01:29:57 +0700

    Implement memoization in fibonacci

--- src/dynamic_programming/fibonacci.rb
+++ src/dynamic_programming/fibonacci.rb
@@ -1,4 +1,6 @@
-def fib(n)
+def fib(n, cache={})
   return 1 if n == 0 || n == 1
-  fib(n - 1) + fib(n - 2)
+  return cache[n] if cache[n]
+
+  cache[n] = fib(n - 1, cache) + fib(n - 2, cache)
 end
--- test/dynamic_programming/fibonacci_test.rb
+++ test/dynamic_programming/fibonacci_test.rb
@@ -3,11 +3,11 @@ require "benchmark"
 require_relative "../../src/dynamic_programming/fibonacci"
 
 class TestMeme < Minitest::Test
-  def test_fibonacci
+  def test_fibonacci_1
     assert_equal 3, fib(3)
   end
 
-  def test_fibonacci
+  def test_fibonacci_2
     how_long = Benchmark.measure do
       assert_equal 1346269, fib(30)
     end
